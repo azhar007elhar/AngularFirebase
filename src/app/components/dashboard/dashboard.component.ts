cf4ca045ce6e6b941283c2145712ef336643bdd5
azhar.elhar <azhar7elhar@gmail.com>
2022-05-14 14:07:36 +0100

    first Commit

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,18 @@
+import { AuthService } from './../../shared/services/auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor(
+    public authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCOBQlXy_j2uqVlpeSwVjOZ9PjzpBLGWpI",
+    authDomain: "fir-authentification-2712b.firebaseapp.com",
+    projectId: "fir-authentification-2712b",
+    storageBucket: "fir-authentification-2712b.appspot.com",
+    messagingSenderId: "946225756048",
+    appId: "1:946225756048:web:cbb55b547490567ec92515",
+    measurementId: "G-L0G8Y28K09"
+  }
 };
 
 /*
